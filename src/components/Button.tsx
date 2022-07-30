3bcad7515178bee4565a3d6feb6f51713054d102
chaospace <ichaospace@gmail.com>
2022-07-30 17:41:31 +0900

    [버튼 스타일 개선]

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   color?: string;
   bold?: boolean;
 }
 
+const ButtonBase = styled.button`
+  border: none;
+  border-radius: 8px;
+  padding: 8px 16px;
+  background-color: orange;
+  color: white;
+  outline: none;
+  text-align: center;
+`;
+
 function Button(props: React.PropsWithChildren<ButtonProps>) {
   const {children, color, bold, ...rest} = props;
   return (
-    <button
+    <ButtonBase
       {...rest}
       style={{
         color: color,
@@ -16,7 +27,7 @@ function Button(props: React.PropsWithChildren<ButtonProps>) {
       }}
     >
       {children}
-    </button>
+    </ButtonBase>
   );
 }
 
