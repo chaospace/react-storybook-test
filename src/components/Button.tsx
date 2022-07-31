8bebbd005ba5502b0c4a83fc077f4ab3236986a6
chaospace <ichaospace@gmail.com>
2022-07-31 19:13:57 +0900

    [SB] 컴포넌트 ArgsTable 주석 적용

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 interface ButtonStyleProps {
+  /** 폰트 컬러 속성 */
   color?: string;
+  /** 볼드 여부 설정 */
   bold?: boolean;
+  /** 버튼 배경 컬러 기본값은 orange */
   bgColor?: string;
+  /** 버튼 보더 설정 여부 */
   stroke?: boolean;
 }
 
@@ -16,12 +20,12 @@ const ButtonBase = styled.button<ButtonStyleProps>`
   border: none;
   border-radius: 8px;
   padding: 8px 16px;
-  color: ${({color}) => color ?? 'white'};
+  color: ${({color}) => color};
   font-weight: ${({bold}) => (bold ? 'bold' : 'normal')};
   border: ${({stroke}) => (stroke ? `3px solid red` : 'none')};
   outline: none;
   text-align: center;
-  background-color: ${({bgColor}) => bgColor ?? 'orange'};
+  background-color: ${({bgColor}) => bgColor};
 `;
 
 /**
@@ -37,5 +41,12 @@ function Button(props: React.PropsWithChildren<ButtonProps>) {
     </ButtonBase>
   );
 }
+
+Button.defaultProps = {
+  color: 'white',
+  bgColor: 'orange',
+  stroke: false,
+  bold: false
+};
 
 export default Button;
